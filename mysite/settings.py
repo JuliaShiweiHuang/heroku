0d4c39855f050bc4908267f4a511072b9e10abed
Shiwei Huang <shiwei.julia.huang@gmail.com>
2022-01-02 18:01:55 -0500

    Fixes for models

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['fierce-beyond-86925.herokuapp.com', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
-    'polls.apps.PollsConfig',
+    'mysite.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
